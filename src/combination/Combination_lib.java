d187c3a531e51aeda6bf6825b481ba16bedb926c
nsjm22 <nishijima0613@yahoo.co.jp>
2016-10-31 17:37:35 +0900

    20161031 submit

---
+++ src/combination/Combination_lib.java
@@ -0,0 +1,15 @@
+package combination;
+
+public class Combination_lib {
+
+	private int s,r;
+	
+	public int getFactorial(int n){
+		int f = 1;
+		for(int i = n; i >= 0; i--){
+			if(i == 0) f *= 1;
+			else f *= i;
+		}
+	    return f;
+	}
+}
